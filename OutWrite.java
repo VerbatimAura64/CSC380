9cf1acdc82550e4b8f9c3c168372ce611a28cd72
bantalek88 <36167320+bantalek88@users.noreply.github.com>
2018-02-27 11:18:21 -0500

    Add files via upload

--- OutWrite.java
+++ OutWrite.java
@@ -5,11 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
-import outwrite.InRead;
+
 
 /**
  *
@@ -19,42 +19,38 @@ public class OutWrite {
 
     public Scanner sc;
 
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-
-        String name, email, password, password1;
-        System.out.println("Please Write Your Name\n-->");
-        name = kb.nextLine();
-        System.out.println("Please Write Your Email address\n-->");
-        email = kb.nextLine();
-        password = verifyPassword();
-        name = properCase(name);
-        // email = verifySyntaxEmail();
-//        System.out.println("Please Enter Your Password\n-->");
-//        password = kb.nextLine();
-//        System.out.println("Please Verify Your Password\n-->");
-//        password1 = kb.nextLine();
-//        if (password != password1) {
-//            System.out.println("Passwords Do Not Match");
-//            verifyPassword();
-//        } else {
-//            System.out.println("Passwords Match");
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner kb = new Scanner(System.in);
+//
+//        String name, email, password, password1;
+//        System.out.println("Please Write Your Name\n-->");
+//        name = kb.nextLine();
+//        System.out.println("Please Write Your Email address\n-->");
+//        email = kb.nextLine();
+//        password = verifyPassword();
+//        name = properCase(name);
+//
+//        try {
+//            //true = append , false = overwrite
+//            FileWriter fw = new FileWriter("customer1.txt", true);
+//            PrintWriter pw = new PrintWriter(fw);
+//            pw.print(getCustID() + ":" + "\t");
+//            pw.print( name + ":" + "\t");
+//            pw.print(email + ":" + "\t");
+//            pw.print(password + ":" + "\t\n");
+//            //GET THIS FORMAT TO WORK
+////            pw.print("CustomerID " + getCustID() + ":" + "\t");
+////            pw.print("Username " + name + ":" + "\t");
+////            pw.print("Useremail " + email + ":" + "\t");
+////            pw.print("Password " + password + ":" + "\t\n");
+//            pw.println();
+//            pw.close();
+//        } catch (IOException e) {
+//            System.out.println("ERROR");
 //        }
-        try {
-            //true = append , false = overwrite
-            FileWriter fw = new FileWriter("customer1.txt", true);
-            PrintWriter pw = new PrintWriter(fw);
-            pw.print(getCustID() + "\t");
-            pw.print(name + "\t");
-            pw.print(email + "\t");
-            pw.print(password + "\t\n");
-            pw.println();
-            pw.close();
-        } catch (IOException e) {
-            System.out.println("ERROR");
-        }
-        System.out.println("File Updated");
-
+//        System.out.println("File Updated");
+openFile();
+readFile();
     }
 
     public static String verifyPassword() {
@@ -64,13 +60,13 @@ public class OutWrite {
         password = kb.nextLine();
         System.out.println("Please Verify Your Password\n-->");
         password1 = kb.nextLine();
-        if (password.equals(password1)) {
+        if (password1.equals(password)) {
             System.out.println("Passwords Match");
         } else {
             System.out.println("Passwords Do Not Match. Please Try Again.");
             verifyPassword();
         }
-        return password;
+        return password1;
     }
 
     public static String properCase(String name) {
@@ -110,43 +106,45 @@ public class OutWrite {
 //        email = kb.nextLine();
 //        return email;
 //    }
-    public static int getCustID() {
+    public static int getCustID() throws FileNotFoundException {
         Random r = new Random();
-
-
+// COLLISON POSSIBLE UNTIL I FIGURE OUT HOW TO AVOID RNG DUPLICATES
         int CustID = r.nextInt(10) + 1;
-//        int MAX = 20;
-//        int numbersNeeded = 0;
-//        if(MAX < numbersNeeded){
-//            throw new IllegalArgumentException("No Number Available");
-//             //Random r = new Random();
-//             Set<Integer> generated = new LinkedHashSet<Integer>();
-//             while (generated.size() < numbersNeeded){
-//                 Integer next = r.nextInt(MAX)+1;
-//                 generated.add(next);
-//                 
-//             }
-//        }        
+//        Scanner custNumChecker = new Scanner(new File("customer1.txt"));
+//        ArrayList<String> custIDList = new ArrayList<String>();
+//        while (custNumChecker.hasNextLine())
+//            custIDList.add(custNumChecker.nextLine());
+//        System.out.println(custIDList);
+//        if (CustID == CustID){
+//            System.out.println("Duplicate");
+//            getCustID();
+            
+//        } 
+            
+    
         return CustID;
     }
 
-    public  void openFile() {
+    public static void openFile() {
         try {
-            sc = new Scanner(new File("customer1.txt"));
+            Scanner s = new Scanner(new File("customer1.txt"));
+            s.close();
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
         }
     }
 
-    public void readFile() {
-        while (sc.hasNext()) {
-            String a = sc.next();
-            String b = sc.next();
-            String c = sc.next();
-            String d = sc.next();
-            System.out.println(a + b + c + d);
-            sc.close();
+    public static void readFile() throws FileNotFoundException {
+        Scanner s = new Scanner(new File("customer1.txt"));
+        while (s.useDelimiter(":").hasNext()) {
+            String a = s.next();
+            String b = s.next();
+            String c = s.next();
+            String d = s.next();
+            //System.out.println(a +" " + b + " " + c + " " + d  + " ");
+            
         }
+       
     }
 
 }
